4f708f70bb9f23367afbe21dd329ed233c6afa0a
sousasag <sousasag@astro.up.pt>
2015-06-04 11:56:13 +0100

    fixing linelists...

--- Differenctial.py
+++ Differenctial.py
@@ -35,8 +35,8 @@ def get_ew(path, line_data):
   create_lines_ew_moog(path+'lines_ew.in', [line_data])
   rp.create_ewfind_par(path,'lines_ew.in')
   rp.run_MOOG(path,'ewfind.par')
-  ew_calc = np.loadtxt(path+'abund_plan_tspec.test', skiprows = 5, usecols = (6,), unpack= True)
-  return ew_calc
+  loggf_out, ew_calc = np.loadtxt(path+'abund_plan_tspec.test', skiprows = 5, usecols = (2, 6), unpack= True)
+  return loggf_out, ew_calc
 
 def calib_loggf(path, line_data, loggf_i, loggf_f):
   print line_data
@@ -51,36 +51,23 @@ def calib_loggf(path, line_data, loggf_i, loggf_f):
     c1 = ( b1 - a1 ) / 2. + a1
     print i, ew_sun, ew_calc, c1
     line_data['loggf'] = c1
-    ew_calc = get_ew(path, line_data)
+    loggf_out, ew_calc = get_ew(path, line_data)
     if ew_calc - ew_sun < 0:
       a1=c1
     else:
       b1=c1
 #  print ew_calc, ew_sun
+#  It is important to be carefull with the use of the loggf vs gf in MOOG"
   if i < 40:
-    return c1
+    return loggf_out
   else:
     line_data['loggf']=loggf
     return 10000+loggf
 
-def recalibrate_loggf_marcs(path, filein, fileout):
-  rp.create_model_marcs(path, 5777, 4.44, 0.0, 1.0)
-  lines_data = read_linelist_file_ew(path,'../linelistDir/full_linelist.dat')
-  for i,line in enumerate(lines_data):
-    print i, len(lines_data)
-    new_loggf = calib_loggf(path, line)
-    line['loggf'] = new_loggf
 
-  fileout = open(fileout,'w')
-  fileout.write('WL          E.P.     loggf    ele      num    EWsun\n')
-  fileout.write('------      -----    -----   ----      ----   -----\n')
-  for datai in lines_data:
-    fileout.write('%7.2f %8.2f  %9.3f %5s %8.1f %8.1f \n' % (float(datai['lambda_rest']),float(datai['EP']),float(datai['loggf']),datai['ele'],float(datai['atom']),float(datai['ew'])))
-  fileout.close()
 
-def recalibrate_loggf_kurucz(path, filein, fileout):
-  rp.create_model_kurucz(path, 5777, 4.44, 0.0, 1.0)
-  lines_data = read_linelist_file_ew(path,'../linelistDir/full_linelist.dat')
+def recalibrate_loggf(path, filein, fileout):
+  lines_data = read_linelist_file_ew(path,filein)
   for i,line in enumerate(lines_data):
     print "--->", i, len(lines_data)
     new_loggf = calib_loggf(path, line, -8., -0.1)
@@ -88,7 +75,6 @@ def recalibrate_loggf_kurucz(path, filein, fileout):
       print "Trying positive numbers"
       new_loggf = calib_loggf(path, line, -0.1, 8.0)
     line['loggf'] = new_loggf
-
   fileout = open(fileout,'w')
   fileout.write('WL          E.P.     loggf    ele      num    EWsun\n')
   fileout.write('------      -----    -----   ----      ----   -----\n')
@@ -96,6 +82,14 @@ def recalibrate_loggf_kurucz(path, filein, fileout):
     fileout.write('%7.2f %8.2f  %9.3f %5s %8.1f %8.1f \n' % (float(datai['lambda_rest']),float(datai['EP']),float(datai['loggf']),datai['ele'],float(datai['atom']),float(datai['ew'])))
   fileout.close()
 
+
+def recalibrate_loggf_marcs(path, filein, fileout):
+  rp.create_model_marcs(path, 5777, 4.44, 0.0, 1.0)
+  recalibrate_loggf(path, filein, fileout)
+
+def recalibrate_loggf_kurucz(path, filein, fileout):
+  rp.create_model_kurucz(path, 5777, 4.44, 0.0, 1.0)
+  recalibrate_loggf(path, filein, fileout)
 
 
   
@@ -104,9 +98,8 @@ def recalibrate_loggf_kurucz(path, filein, fileout):
 ### Main program:
 def main():
   path = 'running_dir/'
-#  recalibrate_loggf_marcs(path,'../linelistDir/full_linelist.dat',path+'out_file')
   recalibrate_loggf_kurucz(path,'../linelistDir/full_linelist.dat',path+'full_calibrated_kur.dat')
-#  recalibrate_loggf_marcs(path,'../linelistDir/full_linelist.dat',path+'full_calibrated_mar.dat')
+  recalibrate_loggf_marcs(path,'../linelistDir/full_linelist.dat',path+'full_calibrated_mar.dat')
 
 #  rp.create_model_marcs(path, 5777, 4.40, 0.0, 1.0)
 #  rp.create_model_kurucz(path, 5777, 4.40, 0.0, 1.0)
